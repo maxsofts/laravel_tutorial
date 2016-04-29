2f5cc86279a7299752e4a37f6d6e8efe8ce081f9
Tran Xuan Duc (Trần Xuân Đức) <ductranxuan.29710@gmail.com>
2016-04-29 16:23:23 +0700

    This set categories

--- laravel/config/database.php
+++ laravel/config/database.php
@@ -56,9 +56,10 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'sample'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', 'root'),
+            'unix_socket'   => '/Applications/MAMP/tmp/mysql/mysql.sock', //this used online mamp mac os
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
