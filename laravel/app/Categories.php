38f8658e24b18a298640f3d66fc19d231b7b57c4
Thanh nc <n.c.thanh0801@gmail.com>
2016-05-04 17:11:46 +0700

    categories,create categories

--- laravel/app/Categories.php
+++ laravel/app/Categories.php
@@ -1,10 +1,20 @@
 <?php
 
 namespace App;
-
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Categories extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'created_at',
+        'slug',
+        'parent',
+        'description',
+    ];
+    public function setCreatedAtAttribute($date){
+        //format l?i $date ðý?c g?i vào t? form
+        $this->attributes['created_at'] = Carbon::createFromFormat('Y-m-d',$date);
+    }
 }
